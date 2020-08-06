2f11f4f7e1c43be7a4e5027ced3fe4cb6e80d049
Romis <romis.nesmelov@gmail.com>
2020-08-06 13:03:55 +0200

    added cors middleware

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -15,8 +15,6 @@ class CorsMiddleware
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST');
+        return $next($request)->header('Access-Control-Allow-Origin', '*');
     }
 }
