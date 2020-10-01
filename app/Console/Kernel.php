d891bb22a3c9a4b4862b318911c5f1b219e11bf3
Romis <romis.nesmelov@gmail.com>
2020-10-01 12:50:48 +0300

    done upload avatar

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->call('CronController@trainingCheck')->daily();
-        $schedule->call('CronController@test')->everyMinute();
+//        $schedule->call('CronController@test')->everyMinute();
     }
 
     /**
