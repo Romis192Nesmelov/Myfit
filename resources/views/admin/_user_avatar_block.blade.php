e0762b320495c0b90a5ed1cfe2f13a46ca23eb70
Romis <romis.nesmelov@gmail.com>
2021-02-19 07:41:02 +0300

    last changes in admin page

--- resources/views/admin/_user_avatar_block.blade.php
+++ resources/views/admin/_user_avatar_block.blade.php
@@ -1,5 +1,5 @@
 @if ($user && $user->avatar)
-    <a class="img-preview" href="{{ asset($user->avatar) }}"><img src="{{ asset($user->avatar) }}" /></a>
+    <a class="img-preview image" href="{{ asset($user->avatar) }}"><img src="{{ asset($user->avatar) }}" /></a>
 @else
     <div class="avatar-icon"><i class="icon-user"></i></div>
 @endif
